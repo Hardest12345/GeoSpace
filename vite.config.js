a96cd25deb844f542f896ce0bffe48748d5daa7e
hardest12345 <mhaidarridho31@gmail.com>
2026-09-05 23:30:48 +0700

    merge: resolve conflicts

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'  // ← Tambahkan ini
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
-    react(),        // ← HARUS ADA untuk React/JSX
+    react(),
     tailwindcss()
   ],
-  // Opsional: tambahkan base path untuk production
   base: './',
-<<<<<<< HEAD
 })
-=======
-})
->>>>>>> d78d614b96abd962ec4be9b622deabda809503b7
